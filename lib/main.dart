0e1461eda4a836cf275dba86121d559a67a63a18
w1w1w1w1 <kapitonovdima979@gmail.com>
2023-05-24 06:12:42 +0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:slot_package/slot_pack/game_menu.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(MaterialApp(
